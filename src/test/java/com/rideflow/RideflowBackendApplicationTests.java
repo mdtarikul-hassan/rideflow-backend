d75ab6431d1a2efdcfcc9965708bbd7c8d0d00bd
tarikul <mdtarikulhassan5432@gmail.com>
2026-08-31 23:57:18 +0530

    application and test file

---
+++ src/test/java/com/rideflow/RideflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
